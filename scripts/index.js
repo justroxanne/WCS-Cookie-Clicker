69c037d284c8ab576742b17e57c1f3b7836eea91
Guillaume Leclout <124732293+MrG-L@users.noreply.github.com>
2023-03-24 09:39:08 +0100

    added darkmode template

--- scripts/index.js
+++ scripts/index.js
@@ -32,10 +32,14 @@ let prices = [5, 10, 15, 20, 25, 30];
 // Tableau des améliorations avec powers[i] = index du helper, et powers[i][j] = amélioration
 // powers[i][0] = credit/clic, powers[i][1] = credits/sec
 
-let powers = [[1, 0], [0, 1], [10, 0], [0, 10], [100, 0], [0, 0]];
-
-
-
+let powers = [
+  [1, 0],
+  [0, 1],
+  [10, 0],
+  [0, 10],
+  [100, 0],
+  [0, 0],
+];
 
 // ---------------------------------------------------------------------------------------------------------------------------style des counters
 //Positionnement des div counter-container et param-container dans le header
@@ -87,28 +91,27 @@ document.querySelector('#data-2').innerHTML = credPerOrganicClickPower;
 
 document.querySelector('#data-3').innerHTML = bank;
 
-
 //Fonction position +1, +2, ...
 const plusPointPosition = (posHoriz, posVert) => {
   let pointAdded = document.createElement('div');
   pointAdded.innerText = `+${credPerOrganicClickPower}`;
   pointAdded.classList.add('pointAdded'); //class qui donne le style et l'animation
-  pointAdded.style.left = `${posHoriz}px`
-  pointAdded.style.top = `${posVert}px`
+  pointAdded.style.left = `${posHoriz}px`;
+  pointAdded.style.top = `${posVert}px`;
   clickWookie.appendChild(pointAdded);
 
   //Purge des div crees
   setTimeout(function () {
     pointAdded.remove();
   }, 3000);
-}
+};
 
 //Fonction d'increment des totaux de click manuel
 const incrementManuelClick = () => {
   OrganicClickTotal++;
   credPerOrganicClickTotal += credPerOrganicClickPower;
   document.querySelector('footer>p').innerText = `${OrganicClickTotal} clicks`;
-}
+};
 
 //creation de la fonction du counter clic/second
 function increase() {
@@ -118,39 +121,36 @@ function increase() {
 }
 setInterval(increase, 1000);
 
-
 //Fonction increment total et insertion au HTML
 const incrementTotalToHtml = () => {
   bank = credPerOrganicClickTotal + credPerSecondTotal;
   document.querySelector('#data-3').innerHTML = bank;
-}
-
-
+};
 
 /*-----------------------------------------Fonction de débloquage d'un helper---------------*/
 
 const unlockHelper = (helperPrice, helper) => {
-  if (bank >= helperPrice){
-    helper.style.filter= 'none';
+  if (bank >= helperPrice) {
+    helper.style.filter = 'none';
   }
-}
+};
 
 const buyHelper = (helperPrice, helper) => {
-  helper.addEventListener('click', function(){
-    if (bank >= helperPrice){
-    bank -= helperPrice;
-    credPerOrganicClickTotal -= helperPrice;
-    document.querySelector('#data-3').innerText = bank;
-    if (helper === helpers[0]){
-      credPerOrganicClickPower ++;
-      document.querySelector('#data-2').innerText = credPerOrganicClickPower;
-    } else if (helper === helpers[1]){
-      credPerSecondPower ++;
-      document.querySelector('#data-1').innerText = credPerSecondPower;
+  helper.addEventListener('click', function () {
+    if (bank >= helperPrice) {
+      bank -= helperPrice;
+      credPerOrganicClickTotal -= helperPrice;
+      document.querySelector('#data-3').innerText = bank;
+      if (helper === helpers[0]) {
+        credPerOrganicClickPower++;
+        document.querySelector('#data-2').innerText = credPerOrganicClickPower;
+      } else if (helper === helpers[1]) {
+        credPerSecondPower++;
+        document.querySelector('#data-1').innerText = credPerSecondPower;
+      }
     }
-    }
-  })
-}
+  });
+};
 
 // const improve = (power) => {
 //   for (let j = 0; j < power.length; j++){
@@ -161,34 +161,27 @@ const buyHelper = (helperPrice, helper) => {
 //   }
 // };
 
-
-
-
-
-
 // -------------------------------------------------------------// Creation du listener sur le wookie (et les helpers) qui incremente le nombre de clic manuel et genere des credits par clic
 document.querySelector('footer>p').innerText = `${OrganicClickTotal} clicks`;
-
 
 clickWookie.addEventListener('click', function (event) {
   event.preventDefault();
 
   let posX = event.offsetX;
   let posY = event.offsetY;
-  
-  plusPointPosition(posX, posY)
+
+  plusPointPosition(posX, posY);
 
   incrementManuelClick();
 
   incrementTotalToHtml();
-  for(let i = 0; i < helpers.length; i++){
+  for (let i = 0; i < helpers.length; i++) {
     unlockHelper(prices[i], helpers[i]);
-    }
+  }
 });
 
-
-  buyHelper(prices[0], helpers[0]);
-  buyHelper(prices[1], helpers[1]);
+buyHelper(prices[0], helpers[0]);
+buyHelper(prices[1], helpers[1]);
 
 // --------------------------------------------------------------------------------------------------------------------------------------------parametre
 // Image Death Star dans paramButton
@@ -210,25 +203,58 @@ returnButton.addEventListener('click', function (e) {
   settingsPanel.style.display = 'none';
 });
 
-// creation de l'evenement click sur la div on darkmode
+// -------------------------------------------------------------------------------------------------------------------------creation du pop up click power
+
+document.getElementById(
+  'ewok-help'
+).innerText = `Power : 1 credit/clic, Price : ${prices[0]} credits`;
+document.getElementById(
+  'C3PO-help'
+).innerText = `Power : 1 credit/second, Price : ${prices[1]} credits`;
+document.getElementById(
+  'R2D2-help'
+).innerText = `Power : 10 credit/clic, Price : ${prices[2]} credits`;
+document.getElementById(
+  'yoda-help'
+).innerText = `Power : 10 credit/second , Price : ${prices[3]} credits`;
+document.getElementById(
+  'xwing-help'
+).innerText = `Power : 100 credit/clic, Price : ${prices[4]} credits`;
+document.getElementById(
+  'falcon-help'
+).innerText = `Power : You win !! , Price : ${prices[5]} credits`;
+
+let ewok = document.querySelector('.ewok');
+let c3po = document.querySelector('.c3po');
+let r2d2 = document.querySelector('.r2d2');
+let yoda = document.querySelector('.yoda');
+let xwing = document.querySelector('.xwing');
+let falcon = document.querySelector('.falcon');
+let bg = document.querySelector('body');
 
 let darkOn = document.querySelector('.sith');
 darkOn.addEventListener('click', function () {
-  clickWookie.innerHTML = `<img src="./FinalImages/kyloRen.png" alt="chewbacca kawaii" width="100%">`;
+  clickWookie.innerHTML = `<img src="./FinalImages/kyloRen.png" alt="KyloRen kawaii" width="100%">`;
+  ewok.innerHTML = `<img src="./FinalImages/stormtrooperbasic.png" alt="stormtrooper kawaii" width="85%">`;
+  c3po.innerHTML = `<img src="./FinalImages/stormTrooper.png" alt="stormTrooper kawaii" width="85%">`;
+  r2d2.innerHTML = `<img src="./FinalImages/ATAT.png" alt="ATAT kawaii" width="65%">`;
+  yoda.innerHTML = `<img src="./FinalImages/TieFighter.png" alt="TieFighter kawaii" width="80%">`;
+  xwing.innerHTML = `<img src="./FinalImages/darthMaul.png" alt="darthMaul kawaii" width="85%">`;
+  falcon.innerHTML = `<img src="./FinalImages/DarthVador.png" alt="DarthVador kawaii" width="85%">`;
+  document.body.style.backgroundImage = "url('/FinalImages/sithBG.jpg')";
 });
 
-// creation de l'evenement click sur la div off
+// creation de l'evenement click sur la div rebel
 
 let forceOn = document.querySelector('.rebel');
 forceOn.addEventListener('click', function () {
-  clickWookie.innerHTML = `<img src="./FinalImages/chewie.png" alt="chewbacca kawaii" width="100%">`;
+  clickWookie.innerHTML = `<img src="./FinalImages/chewie.png" alt="chewbacca kawaii" wi  dth="100%">`;
+  ewok.innerHTML = `<img src="./FinalImages/ewok.png" alt="ewok kawaii" width="85%"></img>`;
+  r2d2.innerHTML = `<img src="./FinalImages/R2D2.png" alt="R2D2 kawaii" width="85%"></img>`;
+  c3po.innerHTML = `<img src="./FinalImages/C3PO.png" alt="C3PO kawaii" width="85%"></img>`;
+  yoda.innerHTML = `<img src="./FinalImages/Yoda.png" alt="yoda kawaii" width="85%">`;
+  xwing.innerHTML = `<img src="./FinalImages/xwing.png" alt="X-wing kawaii" width="85%">`;
+  falcon.innerHTML = `<img src="./FinalImages/Falcon.png" alt="millenium falcon kawaii" width="85%">`;
+  document.body.style.backgroundImage =
+    "url('/FinalImages/background-desktop.jpg')";
 });
-
-// -------------------------------------------------------------------------------------------------------------------------creation du pop up click power
-
-document.getElementById('ewok-help').innerText = `Power : 1 credit/clic, Price : ${prices[0]} credits`;
-document.getElementById('C3PO-help').innerText = `Power : 1 credit/second, Price : ${prices[1]} credits`;
-document.getElementById('R2D2-help').innerText = `Power : 10 credit/clic, Price : ${prices[2]} credits`;
-document.getElementById('yoda-help').innerText = `Power : 10 credit/second , Price : ${prices[3]} credits`;
-document.getElementById('xwing-help').innerText = `Power : 100 credit/clic, Price : ${prices[4]} credits`;
-document.getElementById('falcon-help').innerText = `Power : You win !! , Price : ${prices[5]} credits`;
